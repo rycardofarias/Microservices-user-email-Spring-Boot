5f59c45d617e996070fbaecd5b3ea3a525ffb98b
Ricardo Farias <ricardofariasxyz@gmail.com>
2023-10-29 18:35:52 -0300

    Implementing sending emails with SMTP Gmail

--- email/src/main/java/com/ms/email/consumers/EmailConsumer.java
+++ email/src/main/java/com/ms/email/consumers/EmailConsumer.java
@@ -1,15 +1,26 @@
 package com.ms.email.consumers;
 
 import com.ms.email.dtos.EmailRecordDto;
+import com.ms.email.models.EmailModel;
+import com.ms.email.servicies.EmailSerice;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.BeanUtils;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EmailConsumer {
 
+    final EmailSerice emailSerice;
+
+    public EmailConsumer(EmailSerice emailSerice) {
+        this.emailSerice = emailSerice;
+    }
+
     @RabbitListener(queues = "${broker.queue.email.name}")
     public void listenEmailQueue(@Payload EmailRecordDto emailRecordDto) {
-        System.out.println(emailRecordDto.emailTo());
+        var emailModel = new EmailModel();
+        BeanUtils.copyProperties(emailRecordDto, emailModel);
+        emailSerice.sendEmail(emailModel);
     }
 }
---
+++ email/src/main/java/com/ms/email/servicies/EmailSerice.java
@@ -0,0 +1,51 @@
+package com.ms.email.servicies;
+
+import com.ms.email.enums.StatusEmail;
+import com.ms.email.models.EmailModel;
+import com.ms.email.repositories.EmailRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+@Service
+public class EmailSerice {
+
+   final EmailRepository emailRepository;
+   final JavaMailSender emailSender;
+
+    public EmailSerice(EmailRepository emailRepository, JavaMailSender emailSender) {
+        this.emailRepository = emailRepository;
+        this.emailSender = emailSender;
+    }
+
+    @Value(value = "${spring.mail.username}")
+    private String emailFrom;
+
+    @Transactional
+    public EmailModel sendEmail(EmailModel emailModel) {
+
+        try {
+            emailModel.setSendDateEmail(LocalDateTime.now());
+            emailModel.setEmailFrom(emailFrom);
+
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setTo(emailModel.getEmailTo());
+            message.setSubject(emailModel.getSubject());
+            message.setText(emailModel.getText());
+            emailSender.send(message);
+
+            emailModel.setStatusEmail(StatusEmail.SENT);
+
+        } catch (MailException e){
+            emailModel.setStatusEmail(StatusEmail.ERROR);
+        }
+
+        return emailRepository.save(emailModel);
+    }
+
+}
