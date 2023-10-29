8a1002222c3fcd988c614550d392f94c246ca32d
Ricardo Farias <ricardofariasxyz@gmail.com>
2023-10-29 17:21:47 -0300

    Creating the Consumer

--- email/src/main/java/com/ms/email/configs/RabbitMQConfig.java
+++ email/src/main/java/com/ms/email/configs/RabbitMQConfig.java
@@ -1,12 +1,14 @@
 package com.ms.email.configs;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class RabbiMQConfig {
+public class RabbitMQConfig {
 
     @Value("${broker.queue.email.name}")
     private String queue;
@@ -14,5 +16,11 @@ public class RabbiMQConfig {
     @Bean
     public Queue queue() {
         return new Queue(queue, true);
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return new Jackson2JsonMessageConverter(objectMapper);
     }
 }
