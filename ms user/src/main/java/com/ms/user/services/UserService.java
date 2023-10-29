ad3d081c3d95143aba8f46ec27b7eaf2b539c31c
Ricardo Farias <ricardofariasxyz@gmail.com>
2023-10-29 17:22:02 -0300

    Creating the Porducer

--- ms user/src/main/java/com/ms/user/services/UserService.java
+++ ms user/src/main/java/com/ms/user/services/UserService.java
@@ -1,6 +1,7 @@
 package com.ms.user.services;
 
 import com.ms.user.models.UserModel;
+import com.ms.user.produces.UserProducer;
 import com.ms.user.repositories.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,13 +11,16 @@ public class UserService {
 
 
     final UserRepository userRepository;
-
-    public UserService(UserRepository userRepository) {
+    final UserProducer userProducer;
+    public UserService(UserRepository userRepository, UserProducer userProducer) {
         this.userRepository = userRepository;
+        this.userProducer = userProducer;
     }
 
     @Transactional
     public UserModel save(UserModel userModel){
-        return userRepository.save(userModel);
+        userModel = userRepository.save(userModel);
+        userProducer.publishMessageEmail(userModel);
+        return userModel;
     }
 }
